d60153580c34ff066f9d184f3ac8e847488f4976
HongMJ1315 <gone_co_1008@yahoo.com.tw>
2023-03-29 01:00:14 +0800

    Initial Commit

---
+++ Mancala/MancalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MancalaApp.swift
+//  Mancala
+//
+//  Created by Mr.JB on 2023/3/29.
+//
+
+import SwiftUI
+
+@main
+struct MancalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
